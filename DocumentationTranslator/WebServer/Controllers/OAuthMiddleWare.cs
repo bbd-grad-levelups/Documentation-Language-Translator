37b2230a43994050f08e305763675795147cd6c1
Johan Viljoen <johan.viljoen@bbd.co.za>
2024-04-09 11:18:10 +0200

    OAuth implementation server-side

--- DocumentationTranslator/WebServer/Controllers/OAuthMiddleWare.cs
+++ DocumentationTranslator/WebServer/Controllers/OAuthMiddleWare.cs
@@ -10,29 +10,85 @@ public class OAuthMiddleWare(IHttpClientFactory httpClientFactory) : IMiddleware
 
   public async Task InvokeAsync(HttpContext context, RequestDelegate next)
   {
+  
     var JWTToken = context.Request.Headers.Authorization.ToString();
-
-    // Get the key ID from the JWT header
     string[] parts = JWTToken.Split('.');
+
+    string googlePublicKeysUrl = "https://www.googleapis.com/oauth2/v3/certs";
+    string publicKeysJson = await _httpClientFactory.CreateClient().GetStringAsync(googlePublicKeysUrl);
+
+    bool validation = ValidateToken(publicKeysJson, JWTToken);
+    if (!validation)
+    {
+      return;
+    }
 
     context.Items["UID"] = GetUID(parts[1]);
 
     await next(context);
   }
 
-  static string GetUID(string payload)
+  private static bool ValidateToken(string publicKeysJson, string token)
   {
-    string payloadJson = DecodeBase64Url(payload);
+    string cli_audience;
+    string web_audience;
+    try {
+      cli_audience = Environment.GetEnvironmentVariable("DocServer_CLI_audience") ?? throw new KeyNotFoundException();
+      web_audience = Environment.GetEnvironmentVariable("DocServer_WEB_audience") ?? throw new KeyNotFoundException();
+    }
+    catch (KeyNotFoundException)
+    {
+      Console.WriteLine("Failed to load audience env variables");
+      return false;
+    }
+
+    // WEB
+    try 
+    {
+      var handler = new JwtSecurityTokenHandler();
+      handler.ValidateToken(token, new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKeys = new JsonWebKeySet(publicKeysJson).GetSigningKeys(),
+        ValidIssuer = "https://accounts.google.com",
+        ValidAudience = web_audience
+      }, out var validatedToken);
+      return true;
+    }
+    catch (Exception) {}
+
+    // CLI
+    try 
+    {
+      var handler = new JwtSecurityTokenHandler();
+      handler.ValidateToken(token, new TokenValidationParameters
+      {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        IssuerSigningKeys = new JsonWebKeySet(publicKeysJson).GetSigningKeys(),
+        ValidIssuer = "https://accounts.google.com",
+        ValidAudience = cli_audience
+      }, out var validatedToken);
+      return true;
+    }
+    catch (Exception) {}
+
+    return false;
+  }
+
+  static string GetUID(string base64UrlPayload)
+  {
+    string padded = base64UrlPayload.Length % 4 == 0 ? base64UrlPayload : string.Concat(base64UrlPayload, "====".AsSpan(base64UrlPayload.Length % 4));
+    string base64 = padded.Replace("_", "/").Replace("-", "+");
+    byte[] bytes = Convert.FromBase64String(base64);
+    string payloadJson = Encoding.UTF8.GetString(bytes);
+
     JsonDocument jsonDocument = JsonDocument.Parse(payloadJson);
     JsonElement root = jsonDocument.RootElement;
     return root.GetProperty("sub").ToString();
   }
 
-  static string DecodeBase64Url(string base64Url)
-  {
-    string padded = base64Url.Length % 4 == 0 ? base64Url : string.Concat(base64Url, "====".AsSpan(base64Url.Length % 4));
-    string base64 = padded.Replace("_", "/").Replace("-", "+");
-    byte[] bytes = Convert.FromBase64String(base64);
-    return Encoding.UTF8.GetString(bytes);
-  }
 }
